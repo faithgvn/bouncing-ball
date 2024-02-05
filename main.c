b427310b6755b0468ca81be8116a697f288ecead
faithgvn <faithgvn@gmail.com>
2024-02-06 02:08:59 +0300

    Add right left control

--- main.c
+++ main.c
@@ -4,11 +4,18 @@ int main(int argc, char* argv[])
 {
     InitWindow(800, 600, "Bouncing ball by faithgvn");
 
+    int x = 400;
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawRectangle(0, 0, 800, 490, SKYBLUE);
-        DrawCircle(250, 455, 35, RED);
+        if (IsKeyDown(KEY_RIGHT)) {
+            x = x + 1;
+        }
+        if (IsKeyDown(KEY_LEFT)) {
+            x = x - 1;
+        }
+        DrawCircle(x, 455, 35, RED);
         DrawRectangle(0, 490, 800, 600, GREEN);
         DrawRectangle(0, 500, 800, 600, BROWN);
         EndDrawing();
