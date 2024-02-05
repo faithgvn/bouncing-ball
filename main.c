7858d66bfd8072b745da4611222e2793d36152ee
faithgvn <faithgvn@gmail.com>
2024-02-06 01:55:52 +0300

    Add sky

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawRectangle(0, 0, 800, 490, SKYBLUE);
         DrawCircle(250, 455, 35, RED);
         DrawRectangle(0, 490, 800, 600, GREEN);
         DrawRectangle(0, 500, 800, 600, BROWN);
