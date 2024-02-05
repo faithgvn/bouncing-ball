3bdf3637c7c91d24271a2cdb4bebc5daf3c368af
faithgvn <faithgvn@gmail.com>
2024-02-06 01:42:16 +0300

    Add ground

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 int main(int argc, char* argv[])
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(800, 600, "Bouncing ball by faithgvn");
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawRectangle(0, 490, 800, 600, GREEN);
+        DrawRectangle(0, 500, 800, 600, BROWN);
         EndDrawing();
     }
 
