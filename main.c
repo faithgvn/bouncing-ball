ebec9577f4d2c7fca47b21f49891202276826e0e
faithgvn <faithgvn@gmail.com>
2024-02-07 02:20:55 +0300

    Add natural movement

--- main.c
+++ main.c
@@ -1,52 +1,56 @@
 #include <raylib.h>
-#include <utils.h>
 
 int main(int argc, char* argv[])
 {
     InitWindow(800, 600, "Bouncing ball by faithgvn");
 
-    int x = 400;
-    int y = 455;
-    int vy = 0;
+    double x = 400;
+    double y = 455;
+    double vy = 0;
+    double g = 10;
+    double dt = 1.0 / 60; // 60FPS
+
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawRectangle(0, 0, 800, 490, SKYBLUE);
-        if (IsKeyDown(KEY_RIGHT)) {
-            x = x + 1;
-        }
-        if (IsKeyDown(KEY_LEFT)) {
-            x = x - 1;
-        }
-        if (IsKeyDown(KEY_UP)) {
-            vy = 1;
-        }
+        double t = 0;
+        while (t < 1) {
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawRectangle(0, 0, 800, 490, SKYBLUE);
+            if (IsKeyDown(KEY_RIGHT)) {
+                x = x + 1;
+            }
+            if (IsKeyDown(KEY_LEFT)) {
+                x = x - 1;
+            }
+            if (IsKeyDown(KEY_UP) && y > 454) {
+                vy = -80;
+            }
 
-        if (y < 200) {
-            vy = -1;
-        }
+            if (y < 455 || vy < 0) {
+                y = y + vy * dt;
+                vy = vy + g * dt;
+            }
 
-        TRACELOG(LOG_INFO, "x: %5d \t y: %5d \t vy: %5d", x, y, vy);
+            if (y > 455) {
+                y = 455;
+            }
+            if (y < 35) {
+                y = 35;
+            }
+            if (x > 765) {
+                x = 765;
+            }
+            if (x < 35) {
+                x = 35;
+            }
 
-        y -= vy;
+            DrawCircle(x, y, 35, RED);
+            DrawRectangle(0, 490, 800, 600, GREEN);
+            DrawRectangle(0, 500, 800, 600, BROWN);
+            EndDrawing();
 
-        if (y > 455) {
-            y = 455;
+            t = t + dt;
         }
-        if (y < 35) {
-            y = 35;
-        }
-        if (x > 765) {
-            x = 765;
-        }
-        if (x < 35) {
-            x = 35;
-        }
-
-        DrawCircle(x, y, 35, RED);
-        DrawRectangle(0, 490, 800, 600, GREEN);
-        DrawRectangle(0, 500, 800, 600, BROWN);
-        EndDrawing();
     }
 
     CloseWindow();
